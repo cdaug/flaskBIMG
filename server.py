12460f2223c206aaa435a4ec85804e82222352c1
Colin <cdaug1228@gmail.com>
2015-10-21 09:20:46 -0400

    Updated server.py

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
+from functools import wraps
 from itsdangerous import Signer
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, Response, session, redirect, url_for
 import sqlite3
 
 conn = sqlite3.connect('logins.sqlite')
@@ -20,13 +21,6 @@ Flask.secret_key = "random"
 @app.route("/")
 def index():
     return render_template('index_test.html')
-
-@app.route("/admin")
-def admin():
-    if session['logged_in'] == True:
-        return render_template('admin.html')
-    else:
-        return redirect(url_for('login'))
 
 @app.route("/about")
 def about():
@@ -67,6 +61,35 @@ def briscola():
 @app.route("/test")
 def test():
     return render_template('game_test.html')
+
+
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == 'admin' and password == 'secret'
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
+@app.route('/admin')
+@requires_auth
+def admin():
+    return render_template('admin.html')
+    
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
